2a212dd50262043eb9aa9df90798305536dc3e4d
Teevra Bhandari <teevrabhandari2003@gmail.com>
2024-07-11 00:09:31 +0530

    Ui tried

--- movie/src/components/Pages/Shop.jsx
+++ movie/src/components/Pages/Shop.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import Carousel from '../Carousel/Carousel'
 import HeroSection from './HeroHeading/HeroSection'
 import FindSeriesSection from './FindSeries/Findseries'
+// import { ThreeDCardDemo } from './Test'
+
 
 const Shop = () => {
   return (
     <div>
+
         <HeroSection/>
         <Carousel />
+        {/* <ThreeDCardDemo /> */}
         <FindSeriesSection />
         {/* Video section jo banana hai abhi but kaise vo nhi pata */}
     </div>
